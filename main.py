19ea70951cae2090d3d8852578fda1a72ab1c806
ANDREW <afzm4@mst.edu>
2018-11-04 15:46:36 -0600

    Added files to repo for the first time.

---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Andrew Floyd
+11/4/2018
+CS3001 - HW5 (kNN)
+Dr. Fu
+"""
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score, train_test_split, KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+dtc = DecisionTreeClassifier(random_state=0)
+iris = load_iris()
+cvs = cross_val_score(dtc, iris.data, iris.target, cv=5)
+X = iris.data
+y = iris.target
+kf = KFold(5, True, 1)
+print(kf)
+total_accu = 0
+for train_index, test_index in kf.split(X):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    classifier = KNeighborsClassifier(n_neighbors=5)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    accu = accuracy_score(y_test, y_pred)
+    print(accu)
+    total_accu = total_accu + accu
+    #print(y_pred)
+    #print(y_test)
+
+aver_accu = total_accu/5
+print("Average Performance Accuracy: ", aver_accu)
+#print(cvs)
+
+#x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.20)
+
+#np.savetxt('results.txt', y_pred, delimiter=',')
+
